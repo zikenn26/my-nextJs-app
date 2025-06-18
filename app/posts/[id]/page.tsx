0f6616f53049b30ff31275c1c3001dbe72b367cb
Gulshan Kumar Nayak <gulnayak1206@gmail.com>
2025-06-19 00:17:05 +0530

    Update page.tsx

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,9 +1,15 @@
-import { type PageProps } from "next";
+import React from "react";
 
-export default function PostPage({ params }: PageProps) {
+type PostPageProps = {
+  params: {
+    id: string;
+  };
+};
+
+export default function PostPage({ params }: PostPageProps) {
   return (
     <div>
       <h1>Post ID: {params.id}</h1>
+      {/* Add your post content here */}
     </div>
   );
-}
